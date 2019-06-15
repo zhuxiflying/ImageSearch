7cdeea8e52ce55a65b7942d5486e0e455df84233
zhuxiflying <zhuxiflying2012@gmail.com>
2019-06-15 16:47:12 -0400

    Css grid layout;
    grid overflow;

--- MapReverse/WebContent/script/mapReverse.js
+++ MapReverse/WebContent/script/mapReverse.js
@@ -7,20 +7,18 @@ var imageData;
 d3.json("NateSilver_Example/NateSilver_Example2.json").then(function(data) {
 	imageData = data;
 	loadIconImages();
+});
+
+function loadIconImages() {
+
+	var gallery = d3.select('.map-gallery');
+	
+	var container = gallery.selectAll('icon-image').data(imageData, function(d) {
+		return d.id;
 	});
 
+	container.enter().append('div').attr('class', 'icon-image')
 
-function loadIconImages()
-{
-	var iconMaps = document.getElementById("legend");
-	console.log(iconMaps);
-	for (var i = 0; i < 22; i++) {
-		var div_node = document.createElement("div");
-		div_node.setAttribute("class", "icon-image");
-		var img = document.createElement("IMG");
-		img.src = imageData[i].Image_url;
-		div_node.appendChild(img);	
-		iconMaps.appendChild(div_node);	
-	}
+	container.exit().remove();
 
 }
