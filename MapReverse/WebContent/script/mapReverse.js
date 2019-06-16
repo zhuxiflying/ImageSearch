a639c4d9f7016241e72f69df88426139365e8c13
zhuxiflying <zhuxiflying2012@gmail.com>
2019-06-15 23:12:33 -0400

    Original image and information loading;

--- MapReverse/WebContent/script/mapReverse.js
+++ MapReverse/WebContent/script/mapReverse.js
@@ -17,6 +17,37 @@ function loadIconImages() {
 	var icon_maps = gallery_container.selectAll('.icon-image').data(imageData).enter().append('div').attr('class', 'icon-image');
 
 	//append image element for div container
-	gallery_container.selectAll('.icon-image').append('img').attr('class', 'icon-image').attr('src', function(d) { return d.Image_url; });
+	gallery_container.selectAll('.icon-image').append('img').attr('src', function(d) { return d.Image_url; });
 	
+	
+	gallery_container.selectAll('.icon-image').on("click",loadOriginalImage);
 }
+
+function loadOriginalImage(data){
+	
+
+	d3.select('.origin-image').append('img').attr('class','imageDiv');
+	d3.select('.imageDiv').attr('src', data.OriginImage);
+	
+
+	var labels_keys = Object.keys(data.label);
+	var labels_values = Object.values(data.label);
+	var entities_keys = Object.keys(data.entity);
+	var entities_values = Object.values(data.entity);
+	
+    var textArea = d3.select('.image-info');
+    
+    textArea.html("");
+    
+    textArea.append('p').text("Map Labels:").attr('class','text-title');
+    
+    textArea.selectAll('text-entry').data(labels_keys).enter().append('p').text(function(d) { return d; }).attr('class','text-entry');
+    
+    textArea.append('p').text("Map Entities:").attr('class','text-title');
+    
+    textArea.selectAll('text-entry2').data(entities_keys).enter().append('p').text(function(d) { return d; }).attr('class','text-entry2');
+    
+    textArea.append('p').text("Score:").attr('class','text-title');
+}
+
+
